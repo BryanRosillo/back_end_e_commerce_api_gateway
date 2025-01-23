a2dc6cf9c3c3a37e334d3f8bfe966af3038b53c6
BryanRosillo <108194629+BryanRosillo@users.noreply.github.com>
2025-01-23 17:42:25 -0500

    feat: configuración del microservicio con Spring Cloud Gateway

---
+++ src/test/java/com/ecommerce/backend/gateway/MicroservicioApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.backend.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
